0ce4da844c432d16d7b45f6b024a66caa788604f
Abul Ala  Jobayar <121652371+AbulAlaJobayar@users.noreply.github.com>
2026-03-20 13:26:30 +0600

    Fix typo in getHello return string

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World and pipeline and deployment!';
+    return 'Hello World and pipeline and deploymenttt!';
   }
 }
